d45dfe13b9430139977ca7074838414c28bd6895
ParkSeungMin <qazwsxedc1226@gmail.com>
2023-01-24 15:22:29 +0900

    Feat: Firebase의 remote config 초기 구현
    Firebase의 remote config 기능을 1차적으로 구현하고 사용해 보았다.
    SplashActivity에 이를 적용해 보았다.

---
+++ app/src/main/java/com/example/chatappwithfirebase/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.chatappwithfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/chatappwithfirebase/SplashActivity.kt
@@ -0,0 +1,69 @@
+package com.example.chatappwithfirebase
+
+import android.content.DialogInterface
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+
+class SplashActivity :AppCompatActivity(){
+    private lateinit var linearLayout: LinearLayout
+    lateinit var remoteConfig: FirebaseRemoteConfig
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        linearLayout = findViewById(R.id.splash_activity_linearLayout)
+
+        remoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 0
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.setDefaultsAsync(R.xml.default_config)
+
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val updated = task.result
+                    Log.d("tag1", "Config params updated: $updated")
+                    Toast.makeText(this, "Fetch and activate succeeded",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Fetch failed",
+                        Toast.LENGTH_SHORT).show()
+                }
+                displayMessage()
+
+            }
+
+
+
+    }
+    fun displayMessage(){
+        Log.d("tag1","displayMessage 실행")
+        val splash_background = remoteConfig.getString("splash_background")
+        val caps:Boolean = remoteConfig.getBoolean("splash_message_caps")
+        val splash_message= remoteConfig.getString("splash_message")
+
+        linearLayout.setBackgroundColor(Color.parseColor(splash_background))
+
+        if(caps){
+            val builder:AlertDialog.Builder =  AlertDialog.Builder(this)
+            builder.setMessage(splash_message).setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
+                finish()
+            })
+            builder.create().show()
+        }
+
+    }
+
+
+}
