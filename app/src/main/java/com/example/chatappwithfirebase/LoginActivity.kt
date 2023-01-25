ff9feaca14c50589b9f0e1770c2d23c9b27967b4
ParkSeungMin <qazwsxedc1226@gmail.com>
2023-01-25 16:11:11 +0900

    Feat: 로그인 화면 구성
    LoginActivity를 구성하였다. 로그인 기능과 화면에서의 상호작용은 아직 완료되지 않고, 화면만 개발하였다.

---
+++ app/src/main/java/com/example/chatappwithfirebase/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.example.chatappwithfirebase
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+
+class LoginActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        var mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
+        var login = findViewById<Button>(R.id.loginActivity_button_login)
+        var signin= findViewById<Button>(R.id.loginActivity_button_signin)
+
+        var splash_background = mFirebaseRemoteConfig.getString("splash_background")
+
+        //window.statusBarColor(Color.parseColor(splash_background))
+
+        login.setBackgroundColor(Color.parseColor(splash_background))
+        signin.setBackgroundColor(Color.parseColor(splash_background))
+    }
+}
--- app/src/main/java/com/example/chatappwithfirebase/SplashActivity.kt
+++ app/src/main/java/com/example/chatappwithfirebase/SplashActivity.kt
@@ -1,6 +1,7 @@
 package com.example.chatappwithfirebase
 
 import android.content.DialogInterface
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
@@ -61,6 +62,8 @@ class SplashActivity :AppCompatActivity(){
                 finish()
             })
             builder.create().show()
+        } else{
+            startActivity(Intent(this,LoginActivity::class.java))
         }
 
     }
